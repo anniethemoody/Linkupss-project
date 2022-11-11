f21f9d82b8a0616e881ad020d374a4842e901b92
MrMartellato <eric.yuxiang@gmail.com>
2022-11-11 17:13:47 -0500

    playwright_update

--- zoompythonscript/playwrightdemotest.py
+++ zoompythonscript/playwrightdemotest.py
@@ -8,20 +8,18 @@ currentCommand = "idle"
 
 async def launch():
     global currentCommand
-    print("test1")
     with open('info.dat') as extract:
-        username = extract.readlines(3)
-        URL = extract.readlines(1)
-        password = extract.readlines(2)
+        file = extract.readlines()
+        username = file[2]
+        url = file[0]
+        password = file[1]
     async with async_playwright() as p:
-        print("test2")
         browser = await p.chromium.launch(headless=False, args=['--start-maximized', '--use-fake-ui-for-media-stream'])
-        print("test3")
         page = await browser.new_page(no_viewport=True)
         context = await browser.new_context()
         await context.grant_permissions(permissions=['microphone'])
         await context.grant_permissions(permissions=['camera'])
-        await page.goto(URL)
+        await page.goto(url)
         await asyncio.sleep(0.1)
         await page.locator('#inputname').fill(username)
         await asyncio.sleep(0.1)
